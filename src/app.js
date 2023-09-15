9ce9d61f8086144fc84f8d2e596ab5333b75c8ba
roseeaa <raquelcorreia99@hotmail.com>
2023-09-15 04:26:05 +0100

    added weather forecast JS

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ function formatDate(timestamp) {
   }
   let days = [
     "Sunday",
-    "Monday",
+    " ${day}",
     "Tuesday",
     "Wednesday",
     "Thursday",
@@ -19,6 +19,33 @@ function formatDate(timestamp) {
   ];
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
+}
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = ` <div class ="row"> `;
+  let days = ["Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+     <div class="weather-forecast-date"> ${day}</div>
+     
+     <img
+       src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png"
+       alt=""
+       width="42"
+     />
+     <div class="weather-forecast-temperatures">
+       <span class="weather-forecast-temperature-max"> 18° </span>
+       <span class="weather-forecast-temperature-min"> 12° </span>
+     </div>
+   </div>
+ `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function displayTemperature(response) {
@@ -90,3 +117,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
