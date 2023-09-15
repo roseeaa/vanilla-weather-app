646b65a41d093b0137ff6a9db3d72e95f8c45f63
roseeaa <raquelcorreia99@hotmail.com>
2023-09-15 05:49:57 +0100

    Display the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -21,29 +21,40 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = ` <div class ="row"> `;
-  let days = ["Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
-     <div class="weather-forecast-date"> ${day}</div>
-     
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+     <div class="weather-forecast-date"> ${formatDay(forecastDay.time)}</div>
      <img
-       src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png"
-       alt=""
+       src="${forecastDay.condition.icon_url}"
        width="42"
      />
      <div class="weather-forecast-temperatures">
-       <span class="weather-forecast-temperature-max"> 18° </span>
-       <span class="weather-forecast-temperature-min"> 12° </span>
+       <span class="weather-forecast-temperature-max"> ${Math.round(
+         forecastDay.temperature.maximum
+       )}° </span>
+       <span class="weather-forecast-temperature-min"> ${Math.round(
+         forecastDay.temperature.minimum
+       )}° </span>
      </div>
    </div>
  `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
